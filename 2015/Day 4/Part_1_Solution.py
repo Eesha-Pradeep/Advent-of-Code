6735ea2721605fbd91a1173b5c02ca5742313507
Eesha-Pradeep <151646391+Eesha-Pradeep@users.noreply.github.com>
2024-05-16 07:49:53 +0400

    Add files via upload

---
+++ 2015/Day 4/Part_1_Solution.py
@@ -0,0 +1,12 @@
+import hashlib
+no = 0
+with open("input.txt", "r") as f:
+  lines = f.readlines()
+input = lines[0]
+while True:
+  value = input+str(no)
+  hash = hashlib.md5(value.encode())
+  if (hash.hexdigest()).startswith("0"*5):
+    print(no)
+    break
+  no+=1
